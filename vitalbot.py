ea8d1f45a0d93d29c1d3601a095da13d97cd29dd
Rafa <113555523+RafRojas@users.noreply.github.com>
2025-01-19 15:09:46 +0100

    Update vitalbot.py

--- vitalbot.py
+++ vitalbot.py
@@ -7,12 +7,6 @@ from datetime import datetime, timedelta
 # API Key (Backend Variable)
 OPENAI_API_KEY = st.secrets["openai"]["api_key"]
 
-# Check if it's Linux so it works on Streamlit Cloud
-if os.name == 'posix':
-    __import__('pysqlite3')
-    import sys
-    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, AIMessage
 
